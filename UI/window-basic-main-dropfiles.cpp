5a026256d47d62a2998ed2795f0ebf4762056b6f
desertpogona <desertpogona@gmail.com>
2021-01-28 17:11:52 -0600

    Update window-basic-main-dropfiles.cpp

--- UI/window-basic-main-dropfiles.cpp
+++ UI/window-basic-main-dropfiles.cpp
@@ -15,7 +15,7 @@ using namespace std;
 static const char *textExtensions[] = {"txt", "log", nullptr};
 
 static const char *imageExtensions[] = {"bmp",  "tga", "png",  "jpg",
-					"jpeg", "gif", "webp", nullptr};
+					"jpeg", "gif", nullptr};
 
 static const char *htmlExtensions[] = {"htm", "html", nullptr};
 
